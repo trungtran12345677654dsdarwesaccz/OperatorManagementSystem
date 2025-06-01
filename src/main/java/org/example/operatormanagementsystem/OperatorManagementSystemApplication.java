ff30f21b07f6dcccd7643070caa11cce603fe1e5
Tran Duy Trung <tranduytrung251105@gmail.com>
2025-06-01 22:02:02 +0700

    Replace old project with new one

---
+++ src/main/java/org/example/operatormanagementsystem/OperatorManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package org.example.operatormanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperatorManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/operatormanagementsystem/OperatorManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.operatormanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OperatorManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
