e5e894d392bd8cc1604cebfa22fb49507262ed0e
Phạm Hưng <hungpdhe191672@gmail.com>
2025-06-09 21:50:18 +0700

    merging

--- src/main/java/org/example/operatormanagementsystem/OperatorManagementSystemApplication.java
+++ src/main/java/org/example/operatormanagementsystem/OperatorManagementSystemApplication.java
@@ -2,6 +2,8 @@ package org.example.operatormanagementsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
