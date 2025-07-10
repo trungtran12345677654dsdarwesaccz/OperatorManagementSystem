2ee081a50f9640ce3697fc790faee7a6dcd49dde
Tran Duy Trung <tranduytrung251105@gmail.com>
2025-07-11 04:05:41 +0700

    fix

--- src/main/java/org/example/operatormanagementsystem/OperatorManagementSystemApplication.java
+++ src/main/java/org/example/operatormanagementsystem/OperatorManagementSystemApplication.java
@@ -3,6 +3,7 @@ package org.example.operatormanagementsystem;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -16,7 +17,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 @EntityScan(basePackages = "org.example.operatormanagementsystem.entity")
 public class OperatorManagementSystemApplication {
     public static void main(String[] args) {
-        SpringApplication.run(OperatorManagementSystemApplication.class, args);
+        String port = System.getenv().getOrDefault("PORT", "8080");
+        System.out.println(">>> Using PORT: " + port); // kiểm tra log
+        new SpringApplicationBuilder(OperatorManagementSystemApplication.class)
+                .properties("server.port=" + port)
+                .run(args);
 
 
     }
