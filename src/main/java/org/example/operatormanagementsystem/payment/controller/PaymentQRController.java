8ac081db7bd0f5cc6c7567482c957cd2f8fb2b62
Tran Duy Trung <tranduytrung251105@gmail.com>
2025-07-11 14:00:35 +0700

    f

--- src/main/java/org/example/operatormanagementsystem/payment/controller/PaymentQRController.java
+++ src/main/java/org/example/operatormanagementsystem/payment/controller/PaymentQRController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/payment")
+//@PreAuthorize("hasAnyRole('CUSTOMER')")
 @RequiredArgsConstructor
 public class PaymentQRController {
 
